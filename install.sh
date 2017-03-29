6da8638f227d0ef1b08849cc68bb0ebc7cc6c5f4
David Baynard <davidbaynard@gmail.com>
2017-03-29 12:08:44 +0100

    Ensure only one instance of server runs using lockfile

--- install.sh
+++ install.sh
@@ -2,8 +2,16 @@ echo "cloning crouton clipboard!"
 git clone http://github.com/zwhitchcox/crouton-clipboard ~/.crouton-clipboard
 touch ~/.crouton-clipboard/data.txt
 echo "adding startup script to .bashrc"
-echo "(nohup node ~/.crouton-clipboard/server.js > /dev/null 2>&1 &)                                                                          " >> $HOME/.bashrc
+cat > $HOME/.bashrc <<'EOF'
+# Crouton clipboard# {{{
+( ( (
+   flock -n 9 && (nohup node ~/.crouton-clipboard/server.js > /dev/null 2>&1 &) && echo "Launched crouton clipboard server"
+   ) 9>/var/run/lock/crouton-clipboard.lockfile
+  ) || echo "Crouton clipboard server already running."
+ )
+# }}}
+EOF
 echo "adding copy/paste commands to vimrc"
 echo "nnoremap \"*p :r !cat $HOME/.crouton-clipboard/data.txt<CR>\nvnoremap \"*y :'<,'>w! $HOME/.crouton-clipboard/data.txt<CR>" >> ~/.vimrc
 echo "starting crouton clipboard!"
-(nohup node ~/.crouton-clipboard/server.js > /dev/null 2>&1 &)                                                                          
+source $HOME/.bashrc
